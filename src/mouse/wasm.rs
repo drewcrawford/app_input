244868702be6f6319c1738c12ac98a9fa9aa03ac
Drew Crawford <drew@sealedabstract.com>
2024-12-17 14:38:18 -0600

    Mouse up/down

--- src/mouse/wasm.rs
+++ src/mouse/wasm.rs
@@ -3,10 +3,20 @@ use wasm_bindgen::prelude::*;
 use web_sys::MouseEvent;
 use crate::mouse::{MouseAbsoluteLocation, MouseWindowLocation};
 
+fn js_button_to_rust(button: i16) -> u8 {
+    match button {
+        0 => 0,
+        1 => 2,
+        2 => 1,
+        _ => button as u8,
+    }
+}
+
 #[derive(Debug)]
 pub(crate) struct PlatformCoalescedMouse {
     mouse_listener: JsValue,
-
+    mousedown_listener: JsValue,
+    mouseup_listener: JsValue,
 }
 
 impl PlatformCoalescedMouse {
@@ -16,6 +26,8 @@ impl PlatformCoalescedMouse {
         let document = window.document().expect("no document on window");
 
         let weak = Arc::downgrade(&shared);
+        let weak_down = weak.clone();
+        let weak_up = weak.clone();
 
 
         // Mouse move callback
@@ -39,14 +51,40 @@ impl PlatformCoalescedMouse {
             }
         }) as Box<dyn FnMut(MouseEvent)>);
 
+
         document
             .add_event_listener_with_callback(
                 "mousemove",
                 mousemove_callback.as_ref().unchecked_ref(),
             ).expect("Can't add event listener");
 
+        let mousedown_callback = Closure::wrap(Box::new(move |event: MouseEvent| {
+            if let Some(shared) = weak_down.upgrade() {
+
+                shared.set_key_state(js_button_to_rust(event.button()), true);
+            }
+        }) as Box<dyn FnMut(MouseEvent)>);
+        document.add_event_listener_with_callback(
+            "mousedown",
+            mousedown_callback.as_ref().unchecked_ref(),
+        ).expect("Can't add event listener");
+
+        let mouseup_callback = Closure::wrap(Box::new(move |event: MouseEvent| {
+            if let Some(shared) = weak_up.upgrade() {
+                shared.set_key_state(js_button_to_rust(event.button()), false);
+            }
+
+        }) as Box<dyn FnMut(MouseEvent)>);
+        document.add_event_listener_with_callback(
+            "mouseup",
+            mouseup_callback.as_ref().unchecked_ref(),
+        ).expect("Can't add event listener");
+
+
         Self {
             mouse_listener: mousemove_callback.into_js_value(),
+            mousedown_listener: mousedown_callback.into_js_value(),
+            mouseup_listener: mouseup_callback.into_js_value(),
         }
 
 
