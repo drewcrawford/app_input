826c239eaa88926ba958cb7035b56e40a5e4710b
Drew Crawford <drew@sealedabstract.com>
2024-12-19 01:35:53 -0600

    Keyboard support

--- src/keyboard/linux.rs
+++ src/keyboard/linux.rs
@@ -2,13 +2,14 @@ use std::fs::File;
 use std::os::fd::AsFd;
 use memmap2::MmapMut;
 use std::sync::{Arc, Mutex, OnceLock, Weak};
-use wayland_client::{Connection, Dispatch, Proxy, QueueHandle};
+use wayland_client::{Connection, Dispatch, Proxy, QueueHandle, WEnum};
 use wayland_client::globals::{registry_queue_init, GlobalList, GlobalListContents};
 use wayland_client::protocol::{wl_compositor, wl_pointer, wl_registry, wl_shm};
 use wayland_client::protocol::__interfaces::wl_keyboard_interface;
 use wayland_client::protocol::wl_buffer::WlBuffer;
 use wayland_client::protocol::wl_compositor::WlCompositor;
-use wayland_client::protocol::wl_keyboard::WlKeyboard;
+use wayland_client::protocol::wl_keyboard::{KeyState, WlKeyboard};
+use wayland_client::protocol::wl_keyboard::Event::Key;
 use wayland_client::protocol::wl_pointer::WlPointer;
 use wayland_client::protocol::wl_seat::WlSeat;
 use wayland_client::protocol::wl_shm::{Format, WlShm};
@@ -17,7 +18,8 @@ use wayland_client::protocol::wl_surface::WlSurface;
 use wayland_protocols::xdg::shell::client::xdg_surface::XdgSurface;
 use wayland_protocols::xdg::shell::client::xdg_toplevel::XdgToplevel;
 use wayland_protocols::xdg::shell::client::xdg_wm_base::{Event, XdgWmBase};
-use crate::keyboard::Shared;
+use crate::keyboard::key::KeyboardKey;
+use crate::keyboard::{Keyboard, Shared};
 
 struct KeyboardState {
     shareds: Vec<Weak<Shared>>
@@ -185,7 +187,26 @@ impl Dispatch<WlKeyboard, ()> for AppData {
     fn event(state: &mut Self, proxy: &WlKeyboard, event: <WlKeyboard as Proxy>::Event, data: &(), conn: &Connection, qhandle: &QueueHandle<Self>) {
         match event {
             wayland_client::protocol::wl_keyboard::Event::Key {serial, time, key, state} => {
-                println!("got key event {:?} {:?} {:?}",serial,time,key);
+                if let Some(key) = KeyboardKey::from_vk(key) {
+                    let down = match state {
+                        WEnum::Value(KeyState::Pressed) => {
+                            true
+                        }
+                        WEnum::Value(KeyState::Released) => {
+                            false
+                        }
+                        _ => {
+                            println!("Unknown key state {:?}", state);
+                            false
+                        }
+                    };
+                    KEYBOARD_STATE.get_or_init(Mutex::default).lock().unwrap().apply_all(|shared| {
+                        shared.set_key_state(key, down)
+                    })
+                }
+                else {
+                    println!("Unknown key {key}");
+                }
             }
             _ => println!("got WlKeyboard event {:?}",event)
         }
@@ -243,4 +264,214 @@ pub fn debug_window_show() {
 
 pub fn debug_window_hide() {
     todo!()
+}
+
+
+impl KeyboardKey {
+    fn from_vk(vk: u32) -> Option<Self> {
+        //taken from https://github.com/torvalds/linux/blob/master/include/uapi/linux/input-event-codes.h
+
+        match vk {
+            1 => Some(KeyboardKey::Escape),
+            2 => Some(KeyboardKey::Num1),
+            3 => Some(KeyboardKey::Num2),
+            4 => Some(KeyboardKey::Num3),
+            5 => Some(KeyboardKey::Num4),
+            6 => Some(KeyboardKey::Num5),
+            7 => Some(KeyboardKey::Num6),
+            8 => Some(KeyboardKey::Num7),
+            9 => Some(KeyboardKey::Num8),
+            10 => Some(KeyboardKey::Num9),
+            11 => Some(KeyboardKey::Num0),
+            12 => Some(KeyboardKey::Minus),
+            13 => Some(KeyboardKey::Equal),
+            14 => Some(KeyboardKey::Delete),
+            15 => Some(KeyboardKey::Tab),
+            16 => Some(KeyboardKey::Q),
+            17 => Some(KeyboardKey::W),
+            18 => Some(KeyboardKey::E),
+            19 => Some(KeyboardKey::R),
+            20 => Some(KeyboardKey::T),
+            21 => Some(KeyboardKey::Y),
+            22 => Some(KeyboardKey::U),
+            23 => Some(KeyboardKey::I),
+            24 => Some(KeyboardKey::O),
+            25 => Some(KeyboardKey::P),
+            26 => Some(KeyboardKey::LeftBracket),
+            27 => Some(KeyboardKey::RightBracket),
+            28 => Some(KeyboardKey::Return),
+            29 => Some(KeyboardKey::Control),
+            30 => Some(KeyboardKey::A),
+            31 => Some(KeyboardKey::S),
+            32 => Some(KeyboardKey::D),
+            33 => Some(KeyboardKey::F),
+            34 => Some(KeyboardKey::G),
+            35 => Some(KeyboardKey::H),
+            36 => Some(KeyboardKey::J),
+            37 => Some(KeyboardKey::K),
+            38 => Some(KeyboardKey::L),
+            39 => Some(KeyboardKey::Semicolon),
+            40 => Some(KeyboardKey::Quote),
+            41 => Some(KeyboardKey::Grave),
+            42 => Some(KeyboardKey::Shift),
+            43 => Some(KeyboardKey::Backslash),
+            44 => Some(KeyboardKey::Z),
+            45 => Some(KeyboardKey::X),
+            46 => Some(KeyboardKey::C),
+            47 => Some(KeyboardKey::V),
+            48 => Some(KeyboardKey::B),
+            49 => Some(KeyboardKey::N),
+            50 => Some(KeyboardKey::M),
+            51 => Some(KeyboardKey::Comma),
+            52 => Some(KeyboardKey::Period),
+            53 => Some(KeyboardKey::Slash),
+            54 => Some(KeyboardKey::RightShift),
+            55 => Some(KeyboardKey::KeypadMultiply),
+            56 => Some(KeyboardKey::Option),
+            57 => Some(KeyboardKey::Space),
+            58 => Some(KeyboardKey::CapsLock),
+            59 => Some(KeyboardKey::F1),
+            60 => Some(KeyboardKey::F2),
+            61 => Some(KeyboardKey::F3),
+            62 => Some(KeyboardKey::F4),
+            63 => Some(KeyboardKey::F5),
+            64 => Some(KeyboardKey::F6),
+            65 => Some(KeyboardKey::F7),
+            66 => Some(KeyboardKey::F8),
+            67 => Some(KeyboardKey::F9),
+            68 => Some(KeyboardKey::F10),
+            69 => Some(KeyboardKey::NumLock),
+            70 => Some(KeyboardKey::ScrollLock),
+            71 => Some(KeyboardKey::Keypad7),
+            72 => Some(KeyboardKey::Keypad8),
+            73 => Some(KeyboardKey::Keypad9),
+            74 => Some(KeyboardKey::KeypadMinus),
+            75 => Some(KeyboardKey::Keypad4),
+            76 => Some(KeyboardKey::Keypad5),
+            77 => Some(KeyboardKey::Keypad6),
+            78 => Some(KeyboardKey::KeypadPlus),
+            79 => Some(KeyboardKey::Keypad1),
+            80 => Some(KeyboardKey::Keypad2),
+            81 => Some(KeyboardKey::Keypad3),
+            82 => Some(KeyboardKey::Keypad0),
+            83 => Some(KeyboardKey::KeypadDecimal),
+            //84 ??
+            //85 - KEY_ZENKAKUHANKAKU
+            //86 - KEY_102ND
+            87 => Some(KeyboardKey::F11),
+            88 => Some(KeyboardKey::F12),
+            89 => Some(KeyboardKey::JISUnderscore),
+            //KEY_KATAKANA
+            //KEY_HIRAGANA
+            //KEY_KATAKANAHIRAGANA
+            //KEY_MUHENKAN
+            95 => Some(KeyboardKey::JISKeypadComma),
+            96 => Some(KeyboardKey::KeypadEnter),
+            97 => Some(KeyboardKey::RightControl),
+            98 => Some(KeyboardKey::KeypadDivide),
+            //sysreq
+            100 => Some(KeyboardKey::RightOption),
+            //linefeed
+            102 => Some(KeyboardKey::Home),
+            103 => Some(KeyboardKey::UpArrow),
+            104 => Some(KeyboardKey::PageUp),
+            105 => Some(KeyboardKey::LeftArrow),
+            106 => Some(KeyboardKey::RightArrow),
+            107 => Some(KeyboardKey::End),
+            108 => Some(KeyboardKey::DownArrow),
+            109 => Some(KeyboardKey::PageDown),
+            110 => Some(KeyboardKey::Insert),
+            111 => Some(KeyboardKey::Delete),
+            //macro
+            113 => Some(KeyboardKey::Mute),
+            114 => Some(KeyboardKey::VolumeDown),
+            115 => Some(KeyboardKey::VolumeUp),
+            116 => Some(KeyboardKey::Power),
+            117 => Some(KeyboardKey::KeypadEquals),
+            119 => Some(KeyboardKey::Pause),
+            //scale
+            121 => Some(KeyboardKey::JISKeypadComma),
+            //key_hanguel, hanja,
+            124 => Some(KeyboardKey::JISYen),
+            125 => Some(KeyboardKey::Command),
+            126 => Some(KeyboardKey::RightCommand),
+            //compose
+            128 => Some(KeyboardKey::Stop),
+            129 => Some(KeyboardKey::Again),
+            130 => Some(KeyboardKey::Props),
+            131 => Some(KeyboardKey::Undo),
+            //front
+            133 => Some(KeyboardKey::Copy),
+            134 => Some(KeyboardKey::Open),
+            135 => Some(KeyboardKey::Paste),
+            136 => Some(KeyboardKey::Find),
+            137 => Some(KeyboardKey::Cut),
+            138 => Some(KeyboardKey::Help),
+            139 => Some(KeyboardKey::ContextMenu),
+            //calc
+            //setup
+            //sleep
+            //wakeup
+            //file, sendfile, deletefile
+            //xfer
+            148 => Some(KeyboardKey::LaunchApp1),
+            149 => Some(KeyboardKey::LaunchApp2),
+            150 => Some(KeyboardKey::BrowserHome),
+            //msdos
+            //coffee (lock)
+            //screenlock
+            //rotate display
+            //direction
+            //cycle windows
+            155 => Some(KeyboardKey::LaunchMail),
+            //bookmarks
+            //computer
+            158 => Some(KeyboardKey::BrowserBack),
+            159 => Some(KeyboardKey::BrowserForward),
+            //close cd
+            161 => Some(KeyboardKey::Eject),
+            //eject+close
+            163 => Some(KeyboardKey::NextTrack),
+            164 => Some(KeyboardKey::Play),
+            165 => Some(KeyboardKey::PreviousTrack),
+            166 => Some(KeyboardKey::Stop),
+            //record, rewind, phone, iso, config
+            172 => Some(KeyboardKey::BrowserHome),
+            173 => Some(KeyboardKey::BrowserRefresh),
+            //exit, move, edit, scrollup, scrolldown, kpleftparen, kprightparen,
+            //key new, redo
+            183 => Some(KeyboardKey::F13),
+            184 => Some(KeyboardKey::F14),
+            185 => Some(KeyboardKey::F15),
+            186 => Some(KeyboardKey::F16),
+            187 => Some(KeyboardKey::F17),
+            188 => Some(KeyboardKey::F18),
+            189 => Some(KeyboardKey::F19),
+            190 => Some(KeyboardKey::F20),
+            191 => Some(KeyboardKey::F21),
+            192 => Some(KeyboardKey::F22),
+            193 => Some(KeyboardKey::F23),
+            194 => Some(KeyboardKey::F24),
+
+            200 => Some(KeyboardKey::Play),
+            201 => Some(KeyboardKey::Pause),
+            //prog3, prog4
+            //all applications
+            //dashboard, suspend, close
+            207 => Some(KeyboardKey::Play),
+            //fastforward, bass boost
+            //print
+            //hp, camera, sound, question, email, chat,
+            217 => Some(KeyboardKey::BrowserSearch),
+            //connect, finance, sport, shop
+            //alterase
+            //cancel
+            //brightness up/down
+            226 => Some(KeyboardKey::MediaSelect),
+            _ => None,
+
+
+
+        }
+    }
 }
