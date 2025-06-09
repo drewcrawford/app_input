626560e560abf9f29e4fb3723da2e729cb6d6594
Drew Crawford <drew@sealedabstract.com>
2025-06-09 02:54:34 -0500

    Cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -90,9 +90,9 @@ pub mod linux {
 use windows::Win32::Foundation::{HWND, LPARAM, LRESULT, WPARAM};
 #[cfg(target_os = "windows")]
 pub fn window_proc(hwnd: HWND, msg: u32, w_param: WPARAM, l_param: LPARAM) -> LRESULT {
-    if mouse::windows::window_proc(hwnd, msg, w_param, l_param) == LRESULT(0) {
-        LRESULT(0)
-    } else if keyboard::windows::kbd_window_proc(hwnd, msg, w_param, l_param) == LRESULT(0) {
+    if mouse::windows::window_proc(hwnd, msg, w_param, l_param) == LRESULT(0)
+        || keyboard::windows::kbd_window_proc(hwnd, msg, w_param, l_param) == LRESULT(0)
+    {
         LRESULT(0)
     } else {
         LRESULT(1)
